d19ff64641abf90d46569fbae46122b96382d9b9
Nash <nay225@lehigh.edu>
2026-05-08 16:01:04 -0400

    added in php mailer, finished debugging timelines and added delay

--- function/app.js
+++ function/app.js
@@ -7,10 +7,7 @@ const elementsToObserve = [
   document.querySelector("#name"),
   document.querySelector("#degree"),
   ...document.querySelectorAll(".skill-group"),
-  ...document.querySelectorAll(".card"),
-  ...document.querySelectorAll(".circle"),
-  ...document.querySelectorAll(".Biography"),
-  ...document.querySelectorAll(".slides")
+  ...document.querySelectorAll(".Biography")
 ];
 
 // --------------------
@@ -19,9 +16,10 @@ const elementsToObserve = [
 const observer = new IntersectionObserver((entries) => {
   entries.forEach(entry => {
     entry.target.classList.toggle("animate", entry.isIntersecting);
+    entry.target.classList.toggle("in-view", entry.isIntersecting);
   });
 }, {
-  threshold: 0.2,
+  threshold: 0.5,
   rootMargin: "0px 0px -50px 0px"
 });
 
@@ -59,7 +57,34 @@ form.addEventListener("submit", async (e) => {
   } catch (err) {
     alert("Error sending message.");
   }
-});
+}); 
+
+
+// --------------------
+// DELAY HANDLING
+// --------------------
+
+// Source - https://stackoverflow.com/a/65994348
+// Posted by Mylio Chang
+// Retrieved 2026-05-07, License - CC BY-SA 4.0
+//use nth of type if u want to count a specific card that's a child 
+function generateDelayDivCss() {
+  let cssStrings = [1, 2, 3].map(
+    (n, idx) =>
+      `.timeline.in-view .circle:nth-child(${idx + 1}) {
+      animation-delay: ${idx * 6}s;
+    }`
+  );
+  return cssStrings.join("");
+}
+
+let css = generateDelayDivCss();
+let style = document.createElement("style");
+style.innerHTML = css;
+document.head.appendChild(style);
+// then append the css from the result of the generateDelayDivCss() to the document
+// let css = generateDelayDivCss();
+// create <style> element and append to document
 
 
 
