873dbbf9586b2429dafb3d68d3d688d9adfc3399
Nash <nay225@lehigh.edu>
2026-05-21 12:45:26 -0400

    added in working navbar

--- function/app.js
+++ function/app.js
@@ -6,6 +6,7 @@ const elementsToObserve = [
   document.querySelector(".Intro"),
   document.querySelector("#name"),
   document.querySelector("#degree"),
+  document.querySelector(".title"),
   ...document.querySelectorAll(".skill-group"),
   ...document.querySelectorAll(".Biography")
 ];
@@ -31,60 +32,32 @@ elementsToObserve.forEach(el => {
 // --------------------
 // FORM HANDLING
 // --------------------
-const form = document.getElementById("contact-section");
-const successCard = document.getElementById("success-card");
+const submit_msg = document.querySelectorAll('.response')
+//after 3 seconds it become an opacity of 0
 
-form.addEventListener("submit", async (e) => {
-  e.preventDefault();
+const messages = [
+  document.querySelector("[data-fs-success]"),
+  document.querySelector("[data-fs-error]")
+];
 
-  const formData = new FormData(form);
-
-  try {
-    const response = await fetch(form.action, {
-      method: "POST",
-      body: formData,
-      headers: {
-        Accept: "application/json"
-      }
-    });
-
-    if (!response.ok) throw new Error();
-
-    successCard.classList.remove("hidden");
-    successCard.classList.add("show");
-    form.reset();
-
-  } catch (err) {
-    alert("Error sending message.");
-  }
-}); 
-
+const msg_observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    entry.target.classList.toggle("remove", entry.isIntersecting);
+  })
+});
 
 // --------------------
-// DELAY HANDLING
+// Nav Bar
 // --------------------
 
-// Source - https://stackoverflow.com/a/65994348
-// Posted by Mylio Chang
-// Retrieved 2026-05-07, License - CC BY-SA 4.0
-//use nth of type if u want to count a specific card that's a child 
-function generateDelayDivCss() {
-  let cssStrings = [1, 2, 3].map(
-    (n, idx) =>
-      `.timeline.in-view .circle:nth-child(${idx + 1}) {
-      animation-delay: ${idx * 6}s;
-    }`
-  );
-  return cssStrings.join("");
-}
+const menu = document.getElementById("menu")
 
-let css = generateDelayDivCss();
-let style = document.createElement("style");
-style.innerHTML = css;
-document.head.appendChild(style);
-// then append the css from the result of the generateDelayDivCss() to the document
-// let css = generateDelayDivCss();
-// create <style> element and append to document
+
+
+
+
+
+
 
 
 
